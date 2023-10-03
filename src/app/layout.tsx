a8175d80b7d8bd8e92e7fadeb82a01843d126194
AMO15310 <wachiraamos402@gmail.com>
2023-10-03 18:56:57 +0300

    core:titlr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Amos Dev",
+  title: "Amos Dev - Portfolio",
   description: "Welcome to my creative world",
 };
 
